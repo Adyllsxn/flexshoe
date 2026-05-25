db9863c86a1311dd20c98ebd56a94fd20ba4df00
Adyllsxn <domingos.nxscimento@gmail.com>
2026-05-25 13:43:02 +0100

    feat: adiciona modulos de identidade (Account, Auth, Password, Permission)

--- src/apps/backend/src/app.module.ts
+++ src/apps/backend/src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SystemModule } from './presentation/modules/system/system.module';
 import { PrismaModule } from './infrastructure/prisma/prisma.module';
+import { PermissionModule } from './presentation/modules/identity/permission/permission.module';
+import { PasswordModule } from './presentation/modules/identity/password/password.module';
+import { AuthModule } from './presentation/modules/identity/auth/auth.module';
+import { AccountModule } from './presentation/modules/identity/account/account.module';
 
 @Module({
-  imports: [SystemModule, PrismaModule],
+  imports: [SystemModule, PrismaModule, PermissionModule, PasswordModule, AuthModule, AccountModule],
 })
 export class AppModule {}
